1e4ce4979a7f0371b19207651697d410e86f819a
Raferto <rafid.ferdianto@gmail.com>
2020-10-12 12:12:41 +0700

    html > php

--- quiz1/profile.php
+++ quiz1/profile.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <html>
     <head>
         <title>test</title>
@@ -10,11 +12,11 @@
             <img src="CSS/img/arrow.png" style="width: 5vh;height: auto">
                 <div class="sidenav">
                     <img src="CSS/img/r.jpg" style="width: 10vh;height: auto; padding: 0 0 5vh 10vh">
-                    <a href="index.html">Home</a>
-                    <a href="profile.html">Profile</a>
-                    <a href="hometown.html">hometown</a>
-                    <a href="food.html">Food</a>
-                    <a class="active" href="tourist.html">tourist</a>
+                    <a href="index.php">Home</a>
+                    <a class="active" href="profile.php">Profile</a>
+                    <a href="hometown.php">hometown</a>
+                    <a href="food.php">Food</a>
+                    <a href="tourist.php">tourist</a>
                 </div>
         </div>
 
