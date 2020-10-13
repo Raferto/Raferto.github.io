717b26fa2065ca00d97e8778dd51ee5d3d636da3
Raferto <rafid.ferdianto@gmail.com>
2020-10-13 11:09:35 +0700

    add p-leftbox 1

--- quiz1/JS/script.js
+++ quiz1/JS/script.js
@@ -1,10 +1,10 @@
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       document.getElementById("header").style.height = "35px";
-      document.getElementById("mytab").style.top = "5px";
+      document.getElementById("mytab").style.top = "10px";
     }
     else {
       document.getElementById("header").style.height = "100px";
-      document.getElementById("mytab").style.top = "70px";
+      document.getElementById("mytab").style.top = "75px";
     }
   }
