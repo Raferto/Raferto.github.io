8120afd92ad6c9bdea990ed66e013cf02bfeaad1
Raferto <rafid.ferdianto@gmail.com>
2020-10-14 04:41:24 +0700

    add p.tab & p.aboutMe

--- quiz1/JS/script.js
+++ quiz1/JS/script.js
@@ -1,10 +1,17 @@
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("header").style.height = "35px";
-      document.getElementById("mytab").style.top = "10px";
-    }
-    else {
-      document.getElementById("header").style.height = "100px";
-      document.getElementById("mytab").style.top = "75px";
-    }
+function scrollHeader() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.height = "35px";
+    document.getElementById("mytab").style.top = "10px";
   }
+  else {
+    document.getElementById("header").style.height = "100px";
+    document.getElementById("mytab").style.top = "75px";
+  }
+}
+
+function ClickTab() {
+  setTimeout( function () {  window.scrollTo(window.scrollX, window.scrollY - 40); }, 1);
+}
+// window.addEventListener("hashchange", function () {
+  
+// });
