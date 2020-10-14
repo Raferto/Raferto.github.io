70e42be8328cb7fd31ba2d712192efdc305855a4
Raferto <rafid.ferdianto@gmail.com>
2020-10-15 02:11:20 +0700

    fixing header

--- quiz1/JS/script.js
+++ quiz1/JS/script.js
@@ -1,17 +1,21 @@
 function scrollHeader() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.height = "35px";
-    document.getElementById("mytab").style.top = "10px";
   }
   else {
-    document.getElementById("header").style.height = "100px";
-    document.getElementById("mytab").style.top = "75px";
+    document.getElementById("header").style.height = "200px";
+  }
+}
+
+function scrollTab() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("mytab").style.top = "30px";
+  }
+  else {
+    document.getElementById("mytab").style.top = "175px";
   }
 }
 
 function ClickTab() {
   setTimeout( function () {  window.scrollTo(window.scrollX, window.scrollY - 40); }, 1);
 }
-// window.addEventListener("hashchange", function () {
-  
-// });
